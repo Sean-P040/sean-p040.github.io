b1ca652e514e6c27c0fb2e0eaa96e4ad483d9124
Sean Paul <sean_p040@yahoo.com>
2021-01-11 21:10:39 -0600

    commit2

---
+++ script.js
@@ -0,0 +1,12 @@
+function darkMode() {
+    "use strict";
+    var element = document.body;
+    var element2 = document.getElementById("headerText");
+    var element3 = document.getElementById("navBar");
+    element.classList.toggle("dark-mode");
+    element2.classList.toggle("dark-mode");
+    element3.classList.toggle("dark-mode");
+
+}
+
+
